90a74563b7ec5deefac25a8d85d33ef5ff2388c9
Alexandre <aalcarvalho@celepar.pr.gov.br>
2025-04-28 21:30:55 -0300

    Primeiro commit

---
+++ src/test/java/com/alura/cursospring/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
