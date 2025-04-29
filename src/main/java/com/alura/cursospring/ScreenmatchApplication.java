294604d7dd327fd6c67e20080236096b679930e3
Alexandre <aalcarvalho@celepar.pr.gov.br>
2025-04-28 22:09:05 -0300

    Apontar para api da OMDB e retornar na console

--- src/main/java/com/alura/cursospring/ScreenmatchApplication.java
+++ src/main/java/com/alura/cursospring/ScreenmatchApplication.java
@@ -1,13 +1,23 @@
 package com.alura.cursospring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alura.cursospring.service.ConsumoAPI;
+
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=7638fe5c&t=gilmore+girls&season=1");
+		System.out.println(json);
 	}
 
 }
