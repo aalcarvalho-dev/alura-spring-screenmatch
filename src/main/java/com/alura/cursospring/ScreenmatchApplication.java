b371a52468ac3c3cbe58aecf13573eaa09ff1cb2
Alexandre <aalcarvalho@celepar.pr.gov.br>
2025-04-30 05:31:35 -0300

    Criar interface ConverteDados e classe que implementa

--- src/main/java/com/alura/cursospring/ScreenmatchApplication.java
+++ src/main/java/com/alura/cursospring/ScreenmatchApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alura.cursospring.model.DadosSerie;
 import com.alura.cursospring.service.ConsumoAPI;
+import com.alura.cursospring.service.ConverteDados;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
@@ -16,8 +18,11 @@ public class ScreenmatchApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoAPI();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=7638fe5c&t=gilmore+girls&season=1");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=7638fe5c&t=gilmore+girls");
 		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
 	}
 
 }
